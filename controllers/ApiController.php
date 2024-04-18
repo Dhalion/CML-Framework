4d1b5608d17ea0db1107190e02f5e14694ca0be6
Leon <leon18058r@gmail.com>
2024-04-18 10:36:01 +0200

    Refactor ApiController.php to add caching functionality for API data retrieval

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -1,21 +1,33 @@
 <?php 
 namespace CML\Controllers;
+use CML\Classes\Cache;
 
 class ApiController{
+
+    private Cache $cache;
+
+    public function __construct() {
+        $this->cache = new Cache();
+    }
+
     public function getRepoData($params) {
-        $url = $params['url'];
-    
-        $ch = curl_init();
-    
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    
-        $response = curl_exec($ch);
-        curl_close($ch);
-    
-        $data = json_decode($response, true);
-    
-        return is_array($data) ? $data : false;
+
+        $cacheData = $this->cache->getCacheData("api_data");
+
+        if(!$cacheData){
+            $url = $params['url'];
+            
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $response = curl_exec($ch);
+            curl_close($ch);
+            
+            $data = json_decode($response, true);
+            $this->cache->setCacheData("api_data", $data, $this->cache::DAY_IN_SECONDS);
+            return is_array($data) ? $data : false;
+        }
+        return $cacheData;
     }
     
 }
